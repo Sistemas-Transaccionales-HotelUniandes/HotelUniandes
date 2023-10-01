474b57ff395014c96f2f82388a3657886f5ebba2
Tomas Rodriguez <t.rodriguezp@uniandes.edu.co>
2023-09-30 20:18:11 -0500

    Initial Commit

---
+++ app/src/main/java/uniandes/edu/co/app/HotelUniandesApplication.java
@@ -0,0 +1,12 @@
+package main.java.uniandes.edu.co.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelUniandesApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(HotelUniandesApplication.class, args);
+    }
+}
---
+++ app/src/test/java/uniandes/edu/co/app/HotelUniandesApplicationTests.java
@@ -0,0 +1,12 @@
+package test.java.uniandes.edu.co.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelUniandesApplicationTests {
+    
+	@Test
+	void contextLoads() {
+	}
+}
