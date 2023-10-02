cba711806266162a27c91bcccba63d04c3051e82
Samu2005Inge <sa.hernandezd1@uniandes.edu.co>
2023-10-01 19:02:47 -0500

    creacion repositories

---
+++ app/src/main/java/uniandes/edu/co/app/repositorio/TipoHabitacionRepository.java
@@ -0,0 +1,35 @@
+package uniandes.edu.co.app.repositorio;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import uniandes.edu.co.app.model.TipoHabitacion;
+
+public interface TipoHabitacionRepository extends JpaRepository<TipoHabitacion, Integer> {
+    @Query(value = "SELECT * FROM tipoHabitacion", nativeQuery = true)
+        Collection<TipoHabitacion> darTipoHabitaciones();
+
+    @Query(value = "SELECT * FROM tipoHabitacion WHERE id = :id", nativeQuery = true)
+    TipoHabitacion darTipoHabitacion(@Param("id") Integer id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO tipoHabitacion (id, tipo, descripcion, capacidad, precio) VALUES ( hoteluniandes_sequence.nextval , :tipo, :descripcion, :capacidad, :precio)", nativeQuery = true)
+    void inserTipoUsuario(@Param("tipo") String tipo, @Param("descripcion") String descripcion,@Param("capacidad") Integer capacidad,@Param("precio") Double precio);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE tipoHabitacion SET tipo = :tipo, descripcion = :d, capacidad = :c, precio = :p WHERE id = :id", nativeQuery = true)
+    void actualizarTipoUsuario(@Param("id") Integer id, @Param("tipo") String tipo, @Param("d") String descripcion,@Param("c") Integer capacidad,@Param("p") Double precio);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM tipoHabitacion WHERE id = :id", nativeQuery = true)
+    void eliminarTipoHabitacion(@Param("id") Integer id);
+}
